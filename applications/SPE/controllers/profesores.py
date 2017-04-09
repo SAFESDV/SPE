0f2a9890977a99b3ee69d84760e5ae5a50fe092a
Sergio <regiosen@hotmail.com>
2017-04-09 00:13:19 -0400

    Se agregan todos los datos de auth user con lo que se lee del csv, falta que traduzca las partes de profesor en claves foraneas

--- applications/SPE/controllers/profesores.py
+++ applications/SPE/controllers/profesores.py
@@ -2,6 +2,7 @@
 from Profesores import Profesor
 from usbutils import random_key
 import Encoder
+import csv
 from applications.SPE_lib.modules.grids import simple_spe_grid
 Profesor = Profesor()
 
@@ -60,60 +61,71 @@ def AgregarProfesoresEnMasa():
         archivo = session.archivo_exel
         filepath = os.path.join('uploads', str(archivo))
         with open(filepath) as f:
-            content = f.readlines()
-        for profesor in content:
+            content = csv.reader(f)
+            i=1
+            for profesor in content:
 
-            AtributosProf = str(profesor).split()
-            first_name = AtributosProf[0]
-            last_name = AtributosProf[1]
-            carnet = AtributosProf[2]
-            email = AtributosProf[3]
-            telefono = AtributosProf[4]
-            clave = random_key()
-            auth_User_Id = db.auth_user.insert(
-                first_name=first_name,
-                last_name=last_name,
-                username=carnet,
-                email=email,
-                telefono=telefono,
-                password = db.auth_user.password.validate(clave)
-            )
-            if (len(AtributosProf) == 9):
-                if (AtributosProf[5] == "Asociado"):
-                    categoriaLeida = 1
-                else:
-                    categoriaLeida = 1
-                if (AtributosProf[6] == "Exclusiva"):
-                    dedicacionLeida = 1
-                else:
-                    dedicacionLeida = 1
-                if (AtributosProf[7] == "CienciasDeLosMateriales"):
-                    departamentoLeida = 1
-                else:
-                    departamentoLeida = 1
-                if (AtributosProf[8] == "Sartenejas"):
-                    sedeLeida = 1
-                else:
-                    sedeLeida = 1
-
-                activoLeida = True
-                profesorId = db.Profesor.insert(
-                    usuario=auth_User_Id,
-                    categoria='1',
-                    dedicacion='1',
-                    departamento='1',
-                    sede='1',
-                    activo='True'
+                AtributosProf = profesor
+                first_name = AtributosProf[0]
+                last_name = AtributosProf[1]
+                tipoDocumento = AtributosProf[2]
+                cedula = AtributosProf[3]
+                carnet = AtributosProf[4]
+                email = AtributosProf[5]
+                telefono = AtributosProf[6]
+                direccion = AtributosProf[7]
+                sexo = AtributosProf[8]
+                pais = AtributosProf[9]
+                estado = AtributosProf[10]
+                clave = random_key()
+                auth_User_Id = db.auth_user.insert(
+                    first_name=first_name,
+                    last_name=last_name,
+ #                   tipo_documento=tipoDocumento,
+                    numero_documento=cedula,
+                    username=carnet,
+                    direccion=direccion,
+                    sexo=sexo,
+                    email=email,
+                    telefono=telefono,
+                    password = db.auth_user.password.validate(clave)
                 )
-                profesor=db.Profesor(id=profesorId)
-                group = db.auth_group(role="Profesor")
-                membership = db.auth_membership.insert(
-                    user_id=profesor.usuario,
-                    group_id=group.id,
-                )
-            else:
-                print "linea invalida"
-        
+                if (len(AtributosProf) == 15):
+                    if (AtributosProf[11] == "Asociado"):
+                        categoriaLeida = 1
+                    else:
+                        categoriaLeida = 1
+                    if (AtributosProf[12] == "Exclusiva"):
+                        dedicacionLeida = 1
+                    else:
+                        dedicacionLeida = 1
+                    if (AtributosProf[13] == "CienciasDeLosMateriales"):
+                        departamentoLeida = 1
+                    else:
+                        departamentoLeida = 1
+                    if (AtributosProf[14] == "Sartenejas"):
+                        sedeLeida = 1
+                    else:
+                        sedeLeida = 1
+    
+                    activoLeida = True
+                    profesorId = db.Profesor.insert(
+                        usuario=auth_User_Id,
+                        categoria='1',
+                        dedicacion='1',
+                        departamento='1',
+                        sede='1',
+                        activo='True'
+                    )
+                    profesor=db.Profesor(id=profesorId)
+                    group = db.auth_group(role="Profesor")
+                    membership = db.auth_membership.insert(
+                        user_id=profesor.usuario,
+                        group_id=group.id,
+                    )
+                else:
+                    print "linea invalida: " + str(i)
+                i = i+1
     elif form.errors:
         response.flash = 'form has errors'
     return dict(form=form)
